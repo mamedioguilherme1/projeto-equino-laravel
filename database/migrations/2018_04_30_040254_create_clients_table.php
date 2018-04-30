c28c0d0d95518d4148687d565eef04ba8ede1db8
Guilherme Mamedio <mamedioguilherme@gmail.com>
2018-04-30 01:19:19 -0300

    adicionando tabela clientes

---
+++ database/migrations/2018_04_30_040254_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->string('address', 200);
+            $table->string('cellphone', 20);
+            $table->string('cpf', 20);
+            $table->string('email', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
