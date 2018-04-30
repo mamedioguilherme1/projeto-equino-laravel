0ad5715452708da0db2cd755971482df0c63214c
Guilherme Mamedio <mamedioguilherme@gmail.com>
2018-04-30 01:31:07 -0300

    criando model e controller para a tabela cliente

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Client;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        $clients = Client::all();
+        return view('list-clients', compact('clients'));
+    }
+
+    public function create()
+    {
+        return view('create-client');
+    }
+
+    public function store(Request $request)
+    {
+        $client = new Client;
+        $client->name = $request->name;
+        $client->address = $request->address;
+        $client->cellphone = $request->cellphone;
+        $client->cpf = $request->cpf;
+        $client->email = $request->email;
+        $client->save();
+        return redirect()->route('client.index')->with('message', 'Cliente Criado!');
+    }
+
+    public function show($id){
+        $client = Client::findOrFail($id);
+        return view('list-client-id', compact('client'));
+    }
+
+    public function edit($id)
+    {
+        $client = Client::findOrFail($id);
+        return view('edit-client', compact('client'));
+    }
+
+    public function update(Request $request, $id) {
+        $client = Client::findOrFail($id);
+        $client->name = $request->name;
+        $client->address = $request->address;
+        $client->cellphone = $request->cellphone;
+        $client->cpf = $request->cpf;
+        $client->email = $request->email;
+        $client->save();
+        return redirect()->route('client.index')->with('message', 'Editado com sucesso!');
+
+    }
+
+    public function destroy($id)
+    {
+        $client = Client::findOrFail($id);
+        $client->delete();
+        return redirect()->route('client.index')->with('message', 'Eliminado com sucesso!');
+    }
+}
---
+++ app/Models/Client.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = ['name', 'address', 'cellphone', 'cpf', 'email'];
+    protected $guarded = ['id', 'created_at', 'update_at'];
+    protected $table = 'clients';
+}
