da2ca1f85f6a4ea1d6a8d1f1302cf286055373ef
Guilherme Mamedio <mamedioguilherme@gmail.com>
2018-05-01 00:15:27 -0300

    reajuste database

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-$url = parse_url(getenv('DATABASE_URL'));
+/*$url = parse_url(getenv('DATABASE_URL'));
 $host = $url['host'];
 $username = $url['user'];
 $password = $url['pass'];
-$database = substr($url["path"], 1);
+$database = substr($url["path"], 1);*/
 
 return [
 
@@ -51,21 +51,6 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
-        'mysql_production' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
